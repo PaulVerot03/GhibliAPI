398178f29f9f7e9b75253ae3282757414dcc11c0
PaulVerot03 <paulverot@free.fr>
2023-11-30 01:13:57 +0100

    image

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-/* async function logMovies() {
-    const response = await fetch("https://ghibliapi.vercel.app/films");
-    const movies = await response.json();
-    console.log(movies);
-    //console.log("test");
-
-  }
-logMovies()
-
-console.log(movies.title);
- */
-
-//const axios = require("axios");
-
-//projet similaire mais sur une autre BDD https://github.com/taniarascia/sandbox/tree/master/ghibli
-
-//const apiUrl = "https://ghibliapi.vercel.app/films";
-
-/* axios.get(apiUrl)
-    .then(response => {
-        const movies = response.data;
-        displayMovies(movies);
-    })
-    .catch(error => console.error('Error fetching movies:', error));
- */
-
 fetch("https://ghibliapi.vercel.app/films")
   .then((response) => {
     if (!response.ok) {
@@ -36,15 +10,7 @@ fetch("https://ghibliapi.vercel.app/films")
     displayMovies(movies);
   });
 
-function printsMovies(movies) {
-  movies.forEach((movie) => {
-    console.log("Title:", movie.title);
-    console.log("Image:", movie.image);
-    console.log("Description:", movie.description);
-    console.log("Release Date:", movie.release_date);
-    console.log("------------------------");
-  });
-}
+
 
 function displayMovies(movies) {
   const movieList = document.getElementById("movieList");
@@ -62,19 +28,34 @@ function displayMovies(movies) {
     const h1 = document.createElement("h1");
     h1.textContent = movie.title;
 
+
     const image = document.createElement("img")
     image.setAttribute("src", movie.image);
     image.setAttribute("class", "movieImage");
-    image.setAttribute("style", "width:200px;height:300px;"); // 6:9
 
-    const p = document.createElement("p");
-    movie.description = movie.description.substring(0, 300); //faire bouger au cas ou 
-    p.textContent = `${movie.description}...`;
+    const nomOriginal = document.createElement("p");
+    nomOriginal.setAttribute("class", "original");
+    nomOriginal.textContent = movie.original_title;
+
+    const nomLatin = document.createElement("p");
+    nomLatin.setAttribute("class", "latin");
+    nomLatin.textContent = movie.original_title_romanised;
+
+    const realisateur = document.createElement("p");
+    realisateur.setAttribute("class", "realisateur");
+    realisateur.textContent = movie.director;
+
+    const resume = document.createElement("p");
+    movie.description = movie.description.substring(0, 150); //faire bouger au cas ou 
+    resume.textContent = `${movie.description}...`;
 
     movieList.appendChild(card);
-    card.appendChild(h1);
     card.appendChild(image);
-    card.appendChild(p);
+    card.appendChild(h1);
+    card.appendChild(nomOriginal);
+    card.appendChild(nomLatin);
+    card.appendChild(realisateur);
+    card.appendChild(resume);
     
   });
 }
